3e80ee6e921a6de72196459b391bf41b77ca984e
saidc <saidjoc@gmail.com>
2019-03-01 14:57:30 -0500

    actualizacion

--- server.js
+++ server.js
@@ -5,24 +5,31 @@ var app = express();
 app.get('/', function (req, res) {
   res.sendFile('index.html', {root: path.join(__dirname,"./vistas")});
 });
+
 app.get('/about', function (req, res) {
   res.sendFile('about.html', {root: path.join(__dirname,"./vistas")});
 });
+
 app.get('/contact', function (req, res) {
   res.sendFile('contact.html', {root: path.join(__dirname,"./vistas")});
 });
+
 app.get('/findlocation', function (req, res) {
   res.sendFile('findlocation.html', {root: path.join(__dirname,"./vistas")});
 });
+
 app.get('/result', function (req, res) {
   res.sendFile('result.html', {root: path.join(__dirname,"./vistas")});
 });
+
 app.get('/services', function (req, res) {
   res.sendFile('services.html', {root: path.join(__dirname,"./vistas")});
 });
+
 app.get('/singin', function (req, res) {
   res.sendFile('singin.html', {root: path.join(__dirname,"./vistas")});
 });
+
 app.get('/singup', function (req, res) {
   res.sendFile('singup.html', {root: path.join(__dirname,"./vistas")});
 });
