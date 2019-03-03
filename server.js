1e9ab0fd1199738cbcceed173a70c262bd46c1f4
saidc <saidjoc@gmail.com>
2019-03-02 23:46:30 -0500

    agregando estilos css index.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require("path");
 var app = express();
+app.use(express.static("public"));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html', {root: path.join(__dirname,"./vistas")});
